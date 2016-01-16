c19395758caac69ba9bd7577e24b252118c80888
foolself <foolselfgm@gmail.com>
2016-01-16 23:25:49 +0800

    change

--- hextoten.c
+++ hextoten.c
@@ -6,10 +6,18 @@ int hextoten(char a[5]) {
 	num=0;
 	for(i=0;i<5;i++) {
 		ch=a[i];
-		if((ch=='H')||(ch=='\0')) break;
-		if ((ch>=0)&&(ch<='9')) 
+		printf("====== %d ====\n", i);
+		printf("%c", ch);
+		if((ch>='G')||(ch=='\0')) break;
+		if ((ch>=0)&&(ch<='9')) {
 			ch=ch-'0';
-		else ch=ch-'0'-7; num=num*16+ch;
+			printf("--->%d\n", ch);
+		}
+		else {
+			ch=ch-'7';
+			printf("--->%d\n", ch);
+		}
+		num=num*16+ch;
 	}
 	return(num);
 }
@@ -17,20 +25,19 @@ int hextoten(char a[5]) {
 char *strupr(char *str) 
 { 
 	char *ptr = str; 
-
 	while (*ptr != '\0') { 
 		if (islower(*ptr)) 
 			*ptr = toupper(*ptr); 
 		ptr++; 
 	} 
-
 	return str; 
 }
 
 void main() {
-	char strhex[5]="100";
+	char strhex[5]="abc";
 	int numten,k;
 	strupr(strhex);
+	printf("%s\n", strupr(strhex));
 	numten=hextoten(strhex);
 	printf("NUM=%d\n",numten);
 }
